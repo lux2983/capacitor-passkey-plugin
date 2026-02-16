e0e5e57009688503dff188c70f73266053fef8ba
Lux <albastru@protonmail.com>
2026-02-16 17:48:46 +0100

    fix: align iOS package metadata for capacitor 8

--- Package.swift
+++ Package.swift
@@ -8,9 +8,13 @@ let package = Package(
         .library(
             name: "PasskeyPlugin",
             targets: ["PasskeyPlugin"])
+        ,
+        .library(
+            name: "CapacitorPasskeyPlugin",
+            targets: ["PasskeyPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
     ],
     targets: [
         .target(
